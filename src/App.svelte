f670fcda98e320c67b58469cfce2848a016be0b1
hyunjoebrother <hyunjoe31@gmail.com>
2023-07-27 15:46:30 +0900

    first commit && setting Svelte

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Route, Router } from "svelte-routing";
+  import MainPage from "./routes/MainPage.svelte";
+  export let url = "";
+</script>
+
+<Router {url}>
+  <Route path="/" component={MainPage} />
+</Router>
---
+++ src/components/Button.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let text;
+</script>
+
+<button class="button-container">
+  {text}
+</button>
+
+<style>
+  .button-container {
+    width: 100px;
+    height: 36px;
+    background-color: #000;
+    color: #fff;
+    border-radius: 10px;
+    margin: 10px;
+    cursor: pointer;
+  }
+</style>
